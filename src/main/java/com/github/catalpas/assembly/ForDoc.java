c4094c7444884f2fed5a4a605f22f2568cf906b0
赵万梓 <eric3wade@gmail.com>
2019-08-01 17:00:49 +0800

    第一次release

---
+++ src/main/java/com/github/catalpas/assembly/ForDoc.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2019 the original author or authors.
+ */
+package com.github.catalpas.assembly;
+
+/**
+ * do nothing just for making a -javadoc.jar
+ * @author Zhao Wanzi
+ */
+public class ForDoc {
+    private ForDoc(){}
+}
---
+++ src/main/java/com/github/catalpas/assembly/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * custom maven assembly descriptor
+ * See Also https://github.com/catalpas/catalpa-assembly-descriptor
+ * @author Zhao Wanzi
+ */
+package com.github.catalpas.assembly;
